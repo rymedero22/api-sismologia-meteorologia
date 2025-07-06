61f7b07ad549eb527471d2549cd5181cbba2c268
Daniela Silva <dasilva.22@est.ucab.edu.ve>
2025-07-06 02:07:15 -0400

    endpoints sismos y documentacion + swagger de sismos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { connectMongoose } from "./db/mongoose.js";
+import cors from 'cors';
 
 import weatherRouter from "./routes/weather.route.js";
 import earthquakeRouter from "./routes/earthquake.route.js";
@@ -10,11 +11,13 @@ dotenv.config();
 const app = express()
 const port = 3000
 
+// Middlewares deben ir ANTES de las rutas
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors());
 
 app.use("/weather", weatherRouter);
 app.use("/earthquakes", earthquakeRouter);
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 
 app.listen(port, () => {
